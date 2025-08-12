76c4b81a95cd98c49ea7f8c87adc164fbadc9e1a
adam goldsmith <adam.goldsmith0@gmail.com>
2025-08-12 15:02:24 +0100

    made projects reactive

--- src/component/projects/Projects.tsx
+++ src/component/projects/Projects.tsx
@@ -5,17 +5,20 @@ import './Projects.css';
 
 export default function Projects () {
 
-  return (<div className="projectsContainer">
-    <h1>Projects</h1>
-    {projects.map((proj) => (
-        <Project
-        key={proj.title}
-        title={proj.title}
-        description={proj.description}
-        techStack={proj.techStack}
-        gitLink={proj.gitLink}
-				deployedLink={proj.deployedLink}
-        />
-    ))}
-  </div>);
+  return (
+    <>
+			<h1>Projects</h1>
+			<div className="projectsContainer">
+				{projects.map((proj) => (
+						<Project
+						key={proj.title}
+						title={proj.title}
+						description={proj.description}
+						techStack={proj.techStack}
+						gitLink={proj.gitLink}
+						deployedLink={proj.deployedLink}
+						/>
+				))}
+			</div>
+    </>);
 }
