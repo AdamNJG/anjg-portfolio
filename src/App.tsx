d644b270b945430656822e4ccf25e49f15cec099
adam goldsmith <adam.goldsmith0@gmail.com>
2025-08-12 11:42:23 +0100

    adding basic theme swapping

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,35 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
 import Project from './component/Project'
 
 function App() {
-  //const [count, setCount] = useState(0)
+  const [theme, setTheme] = useState(() => {
+    return localStorage.getItem('theme') || 'light';
+  });
+
+  useEffect(() => {
+    document.documentElement.setAttribute('data-theme', theme);
+    localStorage.setItem('theme', theme);
+  }, [theme]);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(prefers-color-scheme: dark)');
+    const handleChange = () => {
+      if(!localStorage.getItem("theme")) { 
+        setTheme(mediaQuery.matches ? 'dark' : 'light');
+      }
+    };
+    mediaQuery.addEventListener('change', handleChange);
+    return () => mediaQuery.removeEventListener('change', handleChange);
+  }, [])
+
+  const handleThemeChange = () => {
+    setTheme((curr) => (curr === 'light' ? 'dark' : 'light'));
+  }
 
   return (
     <>
+      <button onClick={handleThemeChange}>Theme</button>
       <div>
         <Project title="project title"/>
       </div>
